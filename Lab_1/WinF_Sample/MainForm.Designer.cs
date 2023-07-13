af722a8cbbfbb2003ea33553a7199ddfcbe127e0
Anton Lysenko <lysenko@qarea.com>
2023-07-13 19:48:00 +0300

    Lab_1 sample finished

--- Lab_1/WinF_Sample/MainForm.Designer.cs
+++ Lab_1/WinF_Sample/MainForm.Designer.cs
@@ -37,11 +37,14 @@ namespace WinF_Sample
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_loadPNG = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_loadJPG = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_openPreview = new System.Windows.Forms.ToolStripButton();
+            this.pictureBoxControl = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxControl)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -51,7 +54,7 @@ namespace WinF_Sample
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(544, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -76,6 +79,7 @@ namespace WinF_Sample
             this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
             this.loadToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
             this.loadToolStripMenuItem.Text = "Load...";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.onLoadJPG_Click);
             // 
             // helpToolStripMenuItem
             // 
@@ -96,54 +100,70 @@ namespace WinF_Sample
             this.toolStrip1.AutoSize = false;
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1,
-            this.toolStripButton2,
-            this.toolStripButton3});
+            this.toolStripButton_loadPNG,
+            this.toolStripButton_loadJPG,
+            this.toolStripButton_openPreview});
             this.toolStrip1.Location = new System.Drawing.Point(0, 24);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(44, 426);
+            this.toolStrip1.Size = new System.Drawing.Size(44, 359);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
-            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
             // 
-            // toolStripButton1
+            // toolStripButton_loadPNG
             // 
-            this.toolStripButton1.AutoSize = false;
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-            this.toolStripButton1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(42, 42);
-            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton_loadPNG.AutoSize = false;
+            this.toolStripButton_loadPNG.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton_loadPNG.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_loadPNG.Image")));
+            this.toolStripButton_loadPNG.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_loadPNG.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_loadPNG.Name = "toolStripButton_loadPNG";
+            this.toolStripButton_loadPNG.Size = new System.Drawing.Size(42, 42);
+            this.toolStripButton_loadPNG.Text = "toolStripButton1";
+            this.toolStripButton_loadPNG.Click += new System.EventHandler(this.onLoadJPG_Click);
             // 
-            // toolStripButton2
+            // toolStripButton_loadJPG
             // 
-            this.toolStripButton2.AutoSize = false;
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-            this.toolStripButton2.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(42, 42);
-            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton_loadJPG.AutoSize = false;
+            this.toolStripButton_loadJPG.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton_loadJPG.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_loadJPG.Image")));
+            this.toolStripButton_loadJPG.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_loadJPG.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_loadJPG.Name = "toolStripButton_loadJPG";
+            this.toolStripButton_loadJPG.Size = new System.Drawing.Size(42, 42);
+            this.toolStripButton_loadJPG.Text = "toolStripButton2";
+            this.toolStripButton_loadJPG.Click += new System.EventHandler(this.onLoadPNG_Click);
             // 
-            // toolStripButton3
+            // toolStripButton_openPreview
             // 
-            this.toolStripButton3.AutoSize = false;
-            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
-            this.toolStripButton3.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(42, 42);
-            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton_openPreview.AutoSize = false;
+            this.toolStripButton_openPreview.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton_openPreview.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_openPreview.Image")));
+            this.toolStripButton_openPreview.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_openPreview.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_openPreview.Name = "toolStripButton_openPreview";
+            this.toolStripButton_openPreview.Size = new System.Drawing.Size(42, 42);
+            this.toolStripButton_openPreview.Text = "toolStripButton3";
+            this.toolStripButton_openPreview.Click += new System.EventHandler(this.onOpenPreview_Click);
+            // 
+            // pictureBoxControl
+            // 
+            this.pictureBoxControl.Location = new System.Drawing.Point(47, 27);
+            this.pictureBoxControl.Name = "pictureBoxControl";
+            this.pictureBoxControl.Size = new System.Drawing.Size(497, 356);
+            this.pictureBoxControl.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxControl.TabIndex = 2;
+            this.pictureBoxControl.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(544, 383);
+            this.Controls.Add(this.pictureBoxControl);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -153,6 +173,7 @@ namespace WinF_Sample
             this.menuStrip1.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxControl)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,9 +188,11 @@ namespace WinF_Sample
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
-        private System.Windows.Forms.ToolStripButton toolStripButton2;
-        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton_loadPNG;
+        private System.Windows.Forms.ToolStripButton toolStripButton_loadJPG;
+        private System.Windows.Forms.ToolStripButton toolStripButton_openPreview;
+        private System.Windows.Forms.PictureBox pictureBoxControl;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- Lab_1/WinF_Sample/MainForm.cs
+++ Lab_1/WinF_Sample/MainForm.cs
@@ -12,9 +12,42 @@ namespace WinF_Sample
 {
     public partial class MainForm : Form
     {
+        Image _image;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void loadImage(bool jpg)
+        {
+            // папка, яка буде обрана як початкова у вікні для вибору файлу 
+            openFileDialog1.InitialDirectory = ".";
+
+            // якщо будемо обирати jpg-файли 
+            if (jpg) {
+                // встановлюємо формат файлів для завантаження - jpg 
+                openFileDialog1.Filter = "image (JPEG) files (*.jpg)|*.jpg|All files (*.*)|*.*";
+            }
+            else {
+                // встановлюємо формат файлів для завантаження – png 
+                openFileDialog1.Filter = "image (PNG) files (*.png)|*.png|All files (*.*)|*.*";
+            }
+
+            // якщо відкриття вікна вибору файлу завершилося вибором файлу та натисканням кнопки ОК 
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try {
+                    // намагаємося завантажити файл з ім'ям FileName – обраний користувачем файл. 
+                    _image = Image.FromFile(openFileDialog1.FileName);
+                    // устанавливаем картинку в поле элемента PictureBox 
+                    pictureBoxControl.Image = _image;
+                }
+                catch (Exception ex) {
+                    // показуємо повідомлення з причиною помилки 
+                    MessageBox.Show("Не удалось загрузить файл: " + ex.Message);
+                }
+            }
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -31,9 +64,20 @@ namespace WinF_Sample
                 Application.Exit();
         }
 
-        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
+        private void onLoadJPG_Click(object sender, EventArgs e) {
+            loadImage(true);
+        }
 
+        private void onLoadPNG_Click(object sender, EventArgs e) {
+            loadImage(false);
+        }
+
+        private void onOpenPreview_Click(object sender, EventArgs e)
+        {
+            // створюємо новий екземпляр класу Preview, який відповідає за роботу
+            // з нашою додатковою формою; як параметр ми передаємо наше завантажене зображення 
+            Form PreView = new Preview(_image);
+            PreView.ShowDialog();  // відображуємо форму
         }
     }
 }
